3ba8b464314d6f6a0448025223ef15444e59da76
Carl van Vliet <carl@vvliet.de>
2025-12-17 15:22:55 +0100

    Update Dockerfile, devcontainer.json, .gitignore, and 47 more files

---
+++ lib/pages/landingpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("YourAlternative"),
+        centerTitle: true,
+        backgroundColor: Colors.white
+      ),
+    );
+  }
+}
